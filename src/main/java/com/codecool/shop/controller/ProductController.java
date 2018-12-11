dd335f89a8600f88b3da66f1b50295aeeb1a0b98
kemenykristof <kemenykristof24@gmail.com>
2018-12-11 14:59:40 +0100

    started migrating to bootstrap 4, changed well class to card

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -42,7 +42,7 @@ public class ProductController extends HttpServlet {
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.find(filter));
         context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(filter)));
-        engine.process("product/index.html", context, resp.getWriter());
+        engine.process("product/index_bootstrap.html", context, resp.getWriter());
     }
 
 }
