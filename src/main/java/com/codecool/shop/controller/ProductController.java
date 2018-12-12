924d287a514115dffddd16d5773619f1d66e51ef
boldizsarBalogh <bal.boldizsar@gmail.com>
2018-12-12 13:25:37 +0100

    separated the two filters in the form and implemented a more complex product filter

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -6,7 +6,9 @@ import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.Supplier;
+import com.codecool.shop.model.ProductCategory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,8 +18,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
@@ -29,34 +33,54 @@ public class ProductController extends HttpServlet {
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        Map params = new HashMap<>();
+        SupplierDaoMem supplierdao = SupplierDaoMem.getInstance();
+        List<Supplier> suppliers = supplierdao.getAll();
+        List<ProductCategory> categories = productCategoryDataStore.getAll();
+//        Map params = new HashMap<>();
 //        params.put("category", productCategoryDataStore.find(1));
 //        params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
 //        context.setVariables(params);
+        context.setVariable("categories", categories);
+        context.setVariable("suppliers",suppliers);
+        context.setVariable("recipient", "World");
 
+        int categoryFilter = 1;
+        int supplierFilter = 1;
+        if(req.getParameter("categoryFilter") != null) {
+            categoryFilter = Integer.parseInt(req.getParameter("categoryFilter"));
+        }
+        if(req.getParameter("supplierFilter") != null) {
+            supplierFilter = Integer.parseInt(req.getParameter("supplierFilter"));
+        }
+        if(req.getParameter("supplierFilter") == null && (req.getParameter("categoryFilter") == null)){
+            context.setVariable("category", productCategoryDataStore.find(1));
+            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
 
-        int filter = 1;
-        if(req.getParameter("filter") != null){
-            filter = Integer.parseInt(req.getParameter("filter"));
-            if(filter > 2){
-                context.setVariable("recipient", "World");
-                SupplierDaoMem supplierdao = SupplierDaoMem.getInstance();
-                Supplier supplier = supplierdao.find(filter - 2);
-                context.setVariable("products",productDataStore.getBy(supplier));
-                context.setVariable("category",productCategoryDataStore.find(filter-2));
-            }else{
-                context.setVariable("recipient", "World");
-                context.setVariable("category", productCategoryDataStore.find(filter));
-                context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(filter)));
+        }else if(categoryFilter == 0 && supplierFilter != 0){
+            context.setVariable("category",null);
+            Supplier supplier = supplierdao.find(supplierFilter);
+            context.setVariable("products",productDataStore.getBy(supplier));
+        }else if(categoryFilter != 0 && supplierFilter == 0) {
+            context.setVariable("category", productCategoryDataStore.find(categoryFilter));
+            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(categoryFilter)));
+        }else if(categoryFilter == 0 && supplierFilter == 0){
+            context.setVariable("category", productCategoryDataStore.find(1));
+            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
             }
-
-        }else{
-            context.setVariable("recipient", "World");
-            context.setVariable("category", productCategoryDataStore.find(filter));
-            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(filter)));
+        else{
+            context.setVariable("category",productCategoryDataStore.find(categoryFilter));
+            List<Product> unfilteredProducts = productDataStore.getAll();
+            List<Product> filteredProducts = new ArrayList<>();
+            for(Product product : unfilteredProducts){
+                if(product.getSupplier().getName().equals(supplierdao.find(supplierFilter).getName())){
+                    filteredProducts.add(product);
+                }
+            }
+            context.setVariable("products",filteredProducts);
         }
 
-        engine.process("product/index.html", context, resp.getWriter());
+
+        engine.process("product/index_bootstrap.html", context, resp.getWriter());
     }
 
 }
