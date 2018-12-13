cf37ba4f42a241238a2f7d0e33d52d2e7909891d
boldizsarBalogh <bal.boldizsar@gmail.com>
2018-12-13 11:42:36 +0100

    implemented the eventlistener to store product information in session in the main.js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -2,12 +2,25 @@ function main(){
     let counter = document.querySelector(".badge");
     let addButtons = document.querySelectorAll(".btn");
     for(let button of addButtons){
-        button.addEventListener("click", function (evt) {
-            var value = parseInt(counter.innerHTML);
+        button.addEventListener("click", function () {
+            let value = parseInt(counter.innerHTML);
             value += 1;
             counter.innerHTML = value.toString();
+            let productId = button.dataset.id;
+            updateSession(productId)
+
         })
     }
+    function updateSession(productId) {
+        if(sessionStorage.getItem(productId) === null){
+            sessionStorage.setItem(productId,"1");
+
+        }else{
+            let productCount = sessionStorage.getItem(productId);
+            let temp = parseInt(productCount) + 1;
+            sessionStorage.setItem(productId,temp);
+        }
+    };
 }
 
 main();
