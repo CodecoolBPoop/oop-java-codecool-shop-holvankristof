e2026a2bcb684d149ffc6755f185ce5d7f11c2cd
boldizsarBalogh <bal.boldizsar@gmail.com>
2018-12-13 15:33:05 +0100

    started to implement to send json data via http postrequest not finished.

---
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -0,0 +1,48 @@
+package com.codecool.shop.controller;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.model.ProductCategory;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+
+
+
+@WebServlet(urlPatterns = {"/cart"})
+public class CartController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String f = req.getParameter("json");
+        System.out.println(f);
+
+
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/cart.html", context, resp.getWriter());
+    }
+}
+
+
--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,6 +1,21 @@
 function main(){
     let counter = document.querySelector(".badge");
     let addButtons = document.querySelectorAll(".btn");
+    let cart = document.querySelector(".fa-shopping-cart");
+
+    cart.addEventListener("click",function(){
+        console.log(JSON.stringify(sessionStorage));
+        let post = new XMLHttpRequest();
+        let url = "/cart";
+        let data = new FormData();
+        let json = JSON.stringify(sessionStorage)
+        post.open("POST",url);
+        data.set("products",json);
+        console.log(data);
+        post.send(data);
+        // window.location.replace(url);
+
+    });
     for(let button of addButtons){
         button.addEventListener("click", function () {
             let value = parseInt(counter.innerHTML);
