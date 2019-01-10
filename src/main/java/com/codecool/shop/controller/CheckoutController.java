0355f6c2a13e761e019c0c36b5d1bd3dbb526789
boldizsarBalogh <bal.boldizsar@gmail.com>
2019-01-10 11:23:45 +0100

    Created a checkout page with the server side controller

---
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -0,0 +1,34 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.model.ProductCategory;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.expression.Numbers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.*;
+
+
+@WebServlet(urlPatterns = {"/checkout"})
+public class CheckoutController extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/checkout.html", context, resp.getWriter());
+    }
+}
